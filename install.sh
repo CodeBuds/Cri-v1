b2683c7e04a2f4c598b412ce1457e2b858f83ffb
plunkinguitar <plunkinguitar@gmail.com>
2015-10-07 16:11:08 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+#Directories for all files
+CREATORS="David Smerkous and Eli Smith"
+MODIFIERS="NONE"
+URL="https://raw.github.com/CodeBuds/Cri/master"
+CPREFIX=/usr/local
+CBIN=$CPREFIX/bin
+CLIB=$CPREFIX/lib/cri/
+CCONFIG=$CPREFIX/etc/cri/
+CBREW=$CPREFIX/tmp/cri/
+CDEST=$CBREW/dest
+echo 'Welcom to the Cri installer, this will install Cri in 5 seconds, hit ctrl+z to stop if unwanted'
+sleep 5
