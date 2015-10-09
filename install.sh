c24347b87d45cf4b5badfa205ec100163716a8d8
David Smerkous <smerkousdavid@gmail.com>
2015-10-09 14:45:19 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -109,4 +109,5 @@ echo "Changing the mounts to be root read/write"
 echo "As soon as you mount your system as root, please reboot, the program will ask you in less"
 echo "than 15 seconds"
 sleep 5
-sudo bash rootmount
+sudo chmod +x rootmount
+sudo sh rootmount
