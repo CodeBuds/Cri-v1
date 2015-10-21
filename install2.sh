623c36468a9197766c50e745c971ae8bc3e7f092
David Smerkous <smerkousdavid@gmail.com>
2015-10-20 22:31:29 -0500

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -9,6 +9,29 @@ sleep 1
 echo "Starting..."
 echo
 
+ask() {
+    while true; do
+        if [ "${2:-}" = "Y" ]; then
+            prompt="Y/n"
+            default=Y
+        elif [ "${2:-}" = "N" ]; then
+            prompt="y/N"
+            default=N
+        else
+            prompt="y/n"
+            default=
+        fi
+        read -p "$1 [$prompt] " REPLY </dev/tty
+        if [ -z "$REPLY" ]; then
+            REPLY=$default
+        fi
+        case "$REPLY" in
+            Y*|y*) return 0 ;;
+            N*|n*) return 1 ;;
+        esac
+    done
+}
+
 lineCount()
 {
     wc -l < commands.txt	
