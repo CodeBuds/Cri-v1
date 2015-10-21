05e7fb9e4cb21a7d841105a0f304403c8750442d
David Smerkous <smerkousdavid@gmail.com>
2015-10-20 22:21:39 -0500

    moved

--- install2.sh
+++ install2.sh
@@ -1,104 +1,34 @@
 #!/bin/sh
 
 CBIN=/usr/bin
+CTEMP=~/Downloads/tmp #DO NOT CHANGE THIS TO ~/DOWNLOADS, WANNA KNOW WHY, LOOK AT BOTTOM OF SCRIPT
 
 echo "Welcome to the second part of the installation"
 echo "Developed by David Smerkous and Eli Smith"
 sleep 1
-echo "Changing directories"
+echo "Starting..."
+echo
 
-
-cd ~/Downloads/tmp
-echo "Copying and Deleting commands"
-echo ""
-echo "1/12..."
-sudo mv rootmount $CBIN
-echo "2/12..."
-sudo mv unmount $CBIN
-echo "3/12..."
-sudo mv install $CBIN
-echo "4/12..."
-sudo mv remove $CBIN
-echo "5/12..."
-sudo mv run $CBIN
-echo "6/12..."
-sudo mv search $CBIN
-echo "7/12..."
-sudo mv stop $CBIN
-echo "8/12..."
-sudo mv uninstall $CBIN
-echo "9/12..."
-sudo mv update $CBIN
-echo "10/12..."
-sudo mv reinstall $CBIN
-echo "11/12..."
-sudo mv crishell $CBIN
-echo "12/12..."
-sudo mv cri $CBIN/crosh
-echo "Done copying and deleting main files"
-echo "Moving config files"
-sudo mv fixconfig $CBIN
-sudo mv updatecri $CBIN
-sudo mv acadapkg $CBIN
-sudo mv Xfix $CBIN
-echo "Moving apps"
-sudo mv netlogo $CBIN
-sudo mv thunar $CBIN
-echo "Done"
-ask() {
-    # http://djm.me/ask
-    while true; do
-
-        if [ "${2:-}" = "Y" ]; then
-            prompt="Y/n"
-            default=Y
-        elif [ "${2:-}" = "N" ]; then
-            prompt="y/N"
-            default=N
-        else
-            prompt="y/n"
-            default=
-        fi
-
-        # Ask the question - use /dev/tty in case stdin is redirected from somewhere else
-        read -p "$1 [$prompt] " REPLY </dev/tty
-
-        # Default?
-        if [ -z "$REPLY" ]; then
-            REPLY=$default
-        fi
-
-        # Check if the reply is valid
-        case "$REPLY" in
-            Y*|y*) return 0 ;;
-            N*|n*) return 1 ;;
-        esac
-
-    done
+lineCount()
+{
+    wc -l < commands.txt	
 }
+
 cd $CBIN
-echo "Changing the permissions"
-sudo chmod +x rootmount
-sudo chmod +x unmount
-sudo chmod +x install
-sudo chmod +x remove
-sudo chmod +x run
-sudo chmod +x search
-sudo chmod +x stop
-sudo chmod +x uninstall
-sudo chmod +x update
-sudo chmod +x reinstall
-sudo chmod +x crishell
-sudo chmod +x fixconfig
-sudo chmod +x crosh
-sudo chmod +x updatecri
-sudo chmod +x acadapkg
-sudo chmod +x netlogo
-sudo chmod +x thunar
-sudo chmod +x Xfix
-echo "Done changing permissions"
-echo 
-echo "Please read carefuly below"
+echo "Downloading secondary files"
+sudo wget -q --no-check-certificate "$URL/commands.txt" -O $CTEMP/commands.txt
+sudo chmod 755 commands.txt
+NAMES="$(< commands.txt)" #names from names.txt file
+LINES=$(lineCount)
+NUMBERS=0
+for NAME in $NAMES; do
+    echo "File $NUMBERS/$LINES..."
+    let "NUMBERS += 1"
+    sudo wget -q --no-check-certificate "$URL/$NAME" -O $CBIN/${NAME##*/}
+    sudo chmod 755 ${NAME##*/}
+done
+echo "Done..."
+echo
 
 if ask "Would you like to fix the current config file?"; then
   sudo fixconfig
@@ -107,5 +37,4 @@ fi
 if ask "Would you like to install the academy package? (HIGHLY RECOMMENDED), choose yes for list of packages"; then
   sudo acadapkg
 fi
-cd ~/Downloads
-sudo rm -rf tmp
+sudo rm -rf $CTEMP
