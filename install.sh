71112dc1b6d22e585f0b152abd27dc5ac88f9596
plunkinguitar <plunkinguitar@gmail.com>
2015-11-04 16:07:32 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -58,13 +58,13 @@ if ask "Would you like to install cri?"; then #Goes through the process of insta
 		echo
 		if ask "It looks like xiwi isn't installed would you like to install it (xiwi is a requirement for cri)?"; then
 		    echo "Trying..."
-		    sudo sh crouton -t xiwi,extension,e17
+		    sudo sh crouton -t xiwi,extension
 		fi
 	else
 		echo
 		if ask "It looks like crouton is already installed, we need to update it, is that okay?"; then
 		    echo "Trying..."
-		    sudo sh crouton -t xiwi,extension,e17 -u
+		    sudo sh crouton -t xiwi,extension -u
 		fi	    
 	fi
 else
